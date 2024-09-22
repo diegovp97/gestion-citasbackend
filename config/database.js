f036a2d61354b522c3f4e67d0b8863c19b840a97
diegovp97 <diegovp976@gmail.com>
2024-09-22 17:44:56 +0200

    a

--- config/database.js
+++ config/database.js
@@ -4,12 +4,8 @@ require('dotenv').config(); // Cargar las variables de entorno
 // Crear una instancia de Sequelize con la URL de la base de datos PostgreSQL
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: 'postgres',
-  dialectOptions: {
-    ssl: {
-      require: true, // Esto obliga a usar SSL
-      rejectUnauthorized: false // Esto es para no rechazar certificados autofirmados
+  logging: console.log,
     }
-  }
-});
+  );
 
 module.exports = sequelize;
